c61d3e49935e0cd39c5b2cbf1c82d8615993f3f4
tomikartemik <artemules@mail.ru>
2024-11-27 03:35:18 +0300

    deploy

---
+++ deploy_front.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SESSION_NAME="xouston"
+REPO_DIR="$HOME/front"
+GIT_REPO_URL="https://github.com/54CHA/Xouston-landing.git"
+
+# Функция для выполнения команд в tmux
+tmux_send() {
+    tmux send-keys -t $SESSION_NAME "$1" C-m
+}
+
+if [ ! -d "$REPO_DIR" ]; then
+    git clone $GIT_REPO_URL $REPO_DIR
+fi
+
+cd $REPO_DIR && git pull origin main
+
+tmux kill-session -t $SESSION_NAME
+
+tmux new-session -d -s $SESSION_NAME
+tmux_send "npm install"
+tmux_send "npm run deploy"
